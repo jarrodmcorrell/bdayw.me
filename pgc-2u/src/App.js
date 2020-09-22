e3244c0ef64995238f323a92915173a29ec6a563
Jarrod Correll <jcorrell@Jarrods-MacBook-Pro.local>
2020-09-22 09:49:11 -0400

    added a few features

--- pgc-2u/src/App.js
+++ pgc-2u/src/App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import db from "./firebase";
+
+const divStyle={
+  width:'30%'
+};
+
+
 
 function App() {
+
+  const createCard = () => {
+    const cardName = document.getElementById("outlined-basic").value;
+    const text = document.getElementById("linktext");
+    /** 
+    if(cardName){
+      db.collection("cards").add({
+        name: cardName,
+      });
+    }
+    */
+    //text.innerHTML = cardName;
+  }
+
   return (
     <div className="App">
       <h1>Personalized Greeting Cards 2 U</h1>
-      <div className="titletext">
-        <TextField id="outlined-basic" label="Personalized Title" variant="outlined" />
-      </div>
-      <div className="gsubmit">
-        <Button variant="contained" color="secondary">
-          Get Link
-        </Button>
-      </div>
-      <p className="linktext"></p>
+        <div className="titletext">
+          <TextField id="outlined-basic" style={divStyle} label="Personalized Title" variant="outlined" />
+        </div>
+        <div className="gsubmit">
+          <Button className="card-button" onClick={createCard} variant="contained" color="secondary">
+            Get Link
+          </Button>
+        </div>
+      <a href="https://www.google.com" id="linktext"className="linktext">https://www.google.com</a>
     </div>
   );
 }
--- pgc-2u/src/firebase.js
+++ pgc-2u/src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCgCI-YduaAjftPknc6N_XPqJlSabd6ttY",
     authDomain: "pgc-2u.firebaseapp.com",
@@ -8,4 +10,8 @@ const firebaseConfig = {
     messagingSenderId: "631917577865",
     appId: "1:631917577865:web:6aa7273b47352b3b266858",
     measurementId: "G-QSYFMCPZKL"
-  };
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+export default db;
